eb67c94edf3a53140448cfee80d7a6b21b9246cd
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-09-18 12:56:28 +0700

    Change home.html to sent text in textarea to /analysis

--- thai_text_mock_up/sentiment/urls.py
+++ thai_text_mock_up/sentiment/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from sentiment import views
 urlpatterns = [
-    url(r'^$', views.home, name = 'home'),
-
+    url(r'^$', views.home, name='home'),
+    # url(r'analysis')
 ]
