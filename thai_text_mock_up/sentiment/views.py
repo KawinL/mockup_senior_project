4a3787334ecc55591a0137d32d83de1ea41eaa70
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-09-18 14:08:30 +0700

    get text from url in /sentiment/analysis

--- thai_text_mock_up/sentiment/views.py
+++ thai_text_mock_up/sentiment/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
 
 def home(request):
     return render(request, "input.html")
+
+
+def analysis(request):
+    print(type(request))
+    return HttpResponse(request.GET.get('text'))
