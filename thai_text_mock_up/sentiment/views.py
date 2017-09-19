24cb2d90dd6d0c709356e6a64c978c1c2b313876
Jijy Thamjarat <jijy.chavisa@gmail.com>
2017-09-19 11:55:15 +0700

    add lstm model + fix word embedding function

--- thai_text_mock_up/sentiment/views.py
+++ thai_text_mock_up/sentiment/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
-import word2vec
+import word2vec,sequential_model
+import numpy as np
+# from .sequential_model.lstm import *
 # Create your views here.
 
 
@@ -17,9 +19,23 @@ def analysis(request):
     context = {}
     tokenized_sentences = word2vec.preprocessing.tokenize(text)
     print(tokenized_sentences)
-    sentences_vector = word2vec.preprocessing.word_embedding(tokenized_sentences)
+    sentences_vector = word2vec.preprocessing.word_embedding2(tokenized_sentences)
     print(sentences_vector)
+    prediction= sequential_model.lstm.predict([sentences_vector])
     # context['tokenized'] = tokenized_sentences
     # context['vector'] = sentences_vector
-    context['text'] = text
+    context['text'] = prediction
     return render(request, "output.html", context)
+
+# def analysis2(text):
+#     context = {}
+#     tokenized_sentences = word2vec.preprocessing.tokenize(text)
+#     print(tokenized_sentences)
+#     sentences_vector = word2vec.preprocessing.word_embedding2(tokenized_sentences)
+#     print(str(len(sentences_vector)))
+#     prediction= sequential_model.lstm.predict([sentences_vector])
+#     # context['tokenized'] = tokenized_sentence
+#     # context['vector'] = sentences_vector
+#     context['text'] = prediction
+#     return prediction
+
--- thai_text_mock_up/word2vec/preprocessing.py
+++ thai_text_mock_up/word2vec/preprocessing.py
@@ -1,11 +1,12 @@
 import gensim, deepcut
-
+import os
 def tokenize(string):
     return deepcut.tokenize(string)
 
 def word_embedding(more_sentences):
     # Load & train word2vec model
-    model = gensim.models.Word2Vec.load('/Users/AUM/Documents/PROJECT/mockup_senior_project/thai_text_mock_up/word2vec/w2v')
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    model = gensim.models.Word2Vec.load(BASE_DIR+'/word2vec/w2v')
     model.build_vocab(more_sentences, update=True)
     model.train(more_sentences, total_examples=model.corpus_count, epochs=model.iter)
     sentence_vectors = []
@@ -18,3 +19,20 @@ def word_embedding(more_sentences):
             else:
                 sentence_vectors.append(wordVector)
     return sentence_vectors
+
+def word_embedding2(sentence):
+    # Load & train word2vec model
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    model = gensim.models.Word2Vec.load(BASE_DIR+'/word2vec/w2v')
+    model.build_vocab(sentence, update=True)
+    model.train(sentence, total_examples=model.corpus_count, epochs=model.iter)
+    sentence_vectors = []
+    for word in sentence:
+        try:
+            wordVector = model[word]
+        except KeyError:
+            continue
+        else:
+            sentence_vectors.append(wordVector)
+    return sentence_vectors
+
