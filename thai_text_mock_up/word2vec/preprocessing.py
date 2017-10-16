502a61288f60c72bad0805cd00760927f9e39d42
player <ammarinjtk@outlook.com>
2017-10-16 17:47:27 +0700

    Train w2v with more BEST data (model in Zip file)
    - add .gitignore
    - add w2v visualization (Jupiter notebook)

--- thai_text_mock_up/word2vec/preprocessing.py
+++ thai_text_mock_up/word2vec/preprocessing.py
@@ -1,19 +1,17 @@
 import gensim, deepcut, os, re
 
 def tokenize(text):
-    sentences = re.split(" |\n", text)
-    sentence_list = []
-    for sentence in sentences:
-        if sentence:
-            sentence_list.append(deepcut.tokenize(sentence))
-    return sentence_list
+    # Replace all numbers with zeros
+    text = re.sub('[\d]', '0', text)
+    return [deepcut.tokenize(text)]
 
 def word_embedding(more_sentences):
     # Load & train word2vec model
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    model = gensim.models.Word2Vec.load(BASE_DIR+'/word2vec/w2v')
+    model = gensim.models.Word2Vec.load(BASE_DIR+'/word2vec/w2v.bin')
     model.build_vocab(more_sentences, update=True)
     model.train(more_sentences, total_examples=model.corpus_count, epochs=model.iter)
+    model.save(BASE_DIR+'/word2vec/w2v.bin')
     sentence_vectors = []
     for sentence in more_sentences:
         for word in sentence:
@@ -24,4 +22,3 @@ def word_embedding(more_sentences):
             else:
                 sentence_vectors.append(wordVector)
     return sentence_vectors
-
--- thai_text_mock_up/word2vec/train_w2v.py
+++ thai_text_mock_up/word2vec/train_w2v.py
@@ -1,27 +1,37 @@
 import re, glob, os, gensim
 
-# class MySentences(object):
-#     def __init__(self, dirname):
-#         self.dirname = dirname
-#
-#     def __iter__(self):
-#         for filename in glob.glob(os.path.join(self.dirname, '*.txt')):
-#             file = open(filename, 'r')
-#             line = file.read()
-#             file.close()
-#             line = re.sub(r'<NE>.*<\/NE>', '', line)
-#             line = re.sub(r'<AB>.*<\/AB>', '', line)
-#             pattern = re.compile(r"[\u0E00-\u0E7F0-9.%]+")
-#             yield pattern.findall(line)
+curfilePath = os.path.abspath(__file__)
+curDir = os.path.abspath(os.path.join(curfilePath,os.pardir)) # this will return current directory in which python file resides.
+parentDir = os.path.abspath(os.path.join(curDir,os.pardir)) # this will return parent directory in which python file resides.
+BASE_DIR_TO_FILE = os.path.abspath(os.path.join(parentDir,os.pardir)) # this will return grand-parent directory in which python file resides.
 
-path_list = [
-    "/Users/AUM/Documents/PROJECT/mockup_senior_project/BEST_data/news/",
-    "/Users/AUM/Documents/PROJECT/mockup_senior_project/BEST_data/encyclopedia/",
-    "/Users/AUM/Documents/PROJECT/mockup_senior_project/BEST_data/novel/",
-    "/Users/AUM/Documents/PROJECT/mockup_senior_project/BEST_data/article/"
-]
-all_sentences = []
-for path in path_list:
+best_path_list = [BASE_DIR_TO_FILE + "/BEST2010/Train"]
+best_all_sentences = []
+for path in best_path_list:
+    for filename in glob.glob(os.path.join(path, '*.txt')):
+        file = open(filename, 'r')
+        file_string = file.read()
+        file.close()
+
+        file_string = file_string.replace("\n", "")
+        file_sentence = file_string.split('|')
+        temp_sentence = []
+        for sentence in file_sentence:
+            temp_sentence.append(sentence.split('/')[0])
+        best_all_sentences.append(temp_sentence)
+print("Paid BEST processing done!")
+word_count = []
+for idx, x in enumerate(best_all_sentences):
+    for word in x:
+        word_count.append(word)
+
+best_free_path_list = [
+    BASE_DIR_TO_FILE + "/BEST_data/news/",
+    BASE_DIR_TO_FILE + "/BEST_data/encyclopedia/",
+    BASE_DIR_TO_FILE + "/BEST_data/novel/",
+    BASE_DIR_TO_FILE + "/BEST_data/article/"]
+best_free_all_sentences = []
+for path in best_free_path_list:
     for filename in glob.glob(os.path.join(path, '*.txt')):
         file = open(filename, 'r')
         file_string = file.read()
@@ -29,10 +39,24 @@ for path in path_list:
 
         file_string = re.sub(r'<NE>.*<\/NE>', '', file_string)
         file_string = re.sub(r'<AB>.*<\/AB>', '', file_string)
-        pattern = re.compile(r"[\u0E00-\u0E7F0-9.%]+")
-        all_sentences.append(pattern.findall(file_string))
-print("Preprocessing sentences done!")
-# Train & save word2vec model for sentences
+        file_string = file_string.replace("\n", "")
+        best_free_all_sentences.append(file_string.split('|'))
+print("Free BEST processing done!")
+free_word_count = []
+for idx, x in enumerate(best_free_all_sentences):
+    for word in x:
+        free_word_count.append(word)
+
+print("Preprocessing sentences done!\n\tPaid BEST data : " + str(len(best_all_sentences)) + " files, " + str(len(word_count))
+      + " words.\n\tFree BEST data : " + str(len(best_free_all_sentences)) + " files, " + str(len(free_word_count))
+      + " words.")
+
+all_sentences = best_all_sentences + best_free_all_sentences
+
+# Train word2vec model for sentences
 model = gensim.models.Word2Vec(all_sentences, min_count=1)
 print("Training model done!")
-model.save('/Users/AUM/Documents/PROJECT/mockup_senior_project/thai_text_mock_up/word2vec/w2v')
+
+# Save word2vec model
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model.save(BASE_DIR+'/word2vec/w2v.bin')
